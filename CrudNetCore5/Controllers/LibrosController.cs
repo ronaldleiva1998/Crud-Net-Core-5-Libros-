2706830c3a81c7f8276588a4f85de0b81df2221a
ronaldleiva1998 <ronald_123_1998@hotmail.com>
2022-06-18 15:49:11 -0500

    V1libros

---
+++ CrudNetCore5/Controllers/LibrosController.cs
@@ -0,0 +1,127 @@
+﻿using CrudNetCore5.Data;
+using CrudNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CrudNetCore5.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //http Get Index
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> listLibros = _context.Libro;
+            return View(listLibros);
+        }
+
+        //http Get create
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+        //Http Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha creado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //http Get Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            //Obtener el libro
+            var libro= _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }    
+
+
+
+            return View(libro);
+        }
+
+        //Http Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha actualizado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //http Get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Obtener el libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return View(libro);
+        }
+
+        //Http Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+            var libro= _context.Libro.Find(id);
+
+            if (libro==null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Remove(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha eliminado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ CrudNetCore5/Models/Libro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudNetCore5.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El titulo es obligatorio")]
+        [StringLength(50,ErrorMessage ="El {0} debe ser al menos {2} y maximo {1} caracteres",MinimumLength = 3)]
+        [Display(Name ="Título")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La Fecha es obligatorio")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha Lanzamiento")]
+        public DateTime FechaLanzamiento { get; set; }
+
+        [Required(ErrorMessage = "La autor es obligatorio")]
+        public string Autor { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        public int Precio { get; set; } 
+    }
+}
